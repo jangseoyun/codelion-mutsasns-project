f1fe60c09c0881f04f7322f61c3850fcb0973841
seoyunjang <may-primavera@naver.com>
2022-12-20 11:40:56 +0900

    feat: 프로젝트 생성, GET hello test (22.12.20)

---
+++ src/main/java/com/codelion/mutsasns/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.codelion.mutsasns.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloTest() {
+        return "hello";
+    }
+}
