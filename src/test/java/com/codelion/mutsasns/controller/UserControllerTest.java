ad530f1977464da9770e0275e6098e7fdf89a717
seoyunjang <may-primavera@naver.com>
2022-12-27 00:06:54 +0900

    test: UserController 회원가입 성공여부 테스트
    - 회원가입 성공
    - 회원가입 실패 (userName 중복)

---
+++ src/test/java/com/codelion/mutsasns/controller/UserControllerTest.java
@@ -0,0 +1,76 @@
+package com.codelion.mutsasns.controller;
+
+import com.codelion.mutsasns.domain.user.dto.UserJoinRequest;
+import com.codelion.mutsasns.domain.user.dto.UserJoinResponse;
+import com.codelion.mutsasns.exception.ErrorCode;
+import com.codelion.mutsasns.exception.MutsaAppException;
+import com.codelion.mutsasns.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WithMockUser
+@WebMvcTest(UserController.class)
+@DisplayName("UserControllerTest")
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+    private UserJoinRequest userJoinRequest;
+    private UserJoinResponse userJoinResponse;
+
+    @BeforeEach
+    void setUp() {
+        userJoinRequest = new UserJoinRequest("testUserName", "testPassword");
+        userJoinResponse = new UserJoinResponse(3L, "userName");
+    }
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join_success() throws Exception {
+        when(userService.userCheckAndJoin(any()))
+                .thenReturn(userJoinResponse);
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패(userName 중복)")
+    void join_fail() throws Exception {
+        when(userService.userCheckAndJoin(any()))
+                .thenThrow(new MutsaAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
